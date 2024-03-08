c715e6f27274b6b726b95da5f7598f6cb93da06f
zeinabmohamedattia <zenabmohamed858@gmail.com>
2024-03-08 23:58:44 +0200

    Updates

---
+++ static/js/451.8f69de34.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfinalproject=self.webpackChunkfinalproject||[]).push([[451],{9451:function(e,n,r){r.r(n),r.d(n,{default:function(){return c}});var t=r(184);function c(){return(0,t.jsx)(t.Fragment,{children:(0,t.jsx)("h1",{className:"text-center",children:"All Orders"})})}}}]);
+//# sourceMappingURL=451.8f69de34.chunk.js.map
